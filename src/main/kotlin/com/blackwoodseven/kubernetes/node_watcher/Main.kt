5a8012137d424824984d05d386564814e19a366c
Frederik Nordahl Jul Sabroe <frederikns@gmail.com>
2017-08-30 12:41:58 +0200

    fixed the hazelcast port.

--- src/main/kotlin/com/blackwoodseven/kubernetes/node_watcher/Main.kt
+++ src/main/kotlin/com/blackwoodseven/kubernetes/node_watcher/Main.kt
@@ -41,7 +41,7 @@ fun setupShutdownHandler(leaderLock: ILock) {
 fun initializeHazelcast(): HazelcastInstance {
     val hazelcastConfig = com.hazelcast.config.Config()
     hazelcastConfig.setProperty("hazelcast.discovery.enabled", "true")
-    hazelcastConfig.networkConfig.port = 5000
+    hazelcastConfig.networkConfig.port = 5701
     hazelcastConfig.networkConfig.isPortAutoIncrement = false
     hazelcastConfig.networkConfig.join.multicastConfig.isEnabled = false
     hazelcastConfig.networkConfig.join.tcpIpConfig.isEnabled = false
